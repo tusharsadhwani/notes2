d3aac1574d7f004cc412b04110264eacecff802b
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-20 17:31:28 +0530

    Add HTML preview

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import "./App.css";
 
 import prettier from "prettier";
 import prettierMarkdown from "prettier/parser-markdown";
 
+var remark = require("remark");
+var recommended = require("remark-preset-lint-recommended");
+var html = require("remark-html");
+
 const App = () => {
-  const editorRef = useRef();
   const previewRef = useRef();
 
   const parse = () => {
@@ -17,14 +20,29 @@ const App = () => {
     setCode(formattedCode);
   };
 
+  const preview = (code) => {
+    remark()
+      .use(recommended)
+      .use(html)
+      .process(code, function (err, file) {
+        if (!err) {
+          const html = String(file);
+          previewRef.current.innerHTML = html;
+        }
+      });
+  };
+
   const [code, setCode] = useState("");
+
+  const handleChange = (event) => {
+    const newCode = event.target.value;
+    setCode(newCode);
+    preview(newCode);
+  };
 
   return (
     <div className="App">
-      <textarea
-        onChange={(e) => setCode(e.target.value)}
-        value={code}
-      ></textarea>
+      <textarea onChange={handleChange} value={code}></textarea>
       <div ref={previewRef}></div>
       <button onClick={parse}>Prettify</button>
     </div>
