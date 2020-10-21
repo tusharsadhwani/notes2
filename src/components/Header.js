ec19b247fe72a3359212b25a27452bebadbf0081
Aastha Gupta <18bit102@ietdavv.edu.in>
2020-10-21 17:39:04 +0530

    Add fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Header.module.css"
 
 function Header(){
-    return <div className={classes.header}>Q U I L L</div>
+    return <div className={classes.header}>Quill</div>
 }
 
 export default Header;
