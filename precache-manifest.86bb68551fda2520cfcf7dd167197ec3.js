175ad10b05761b343057dac79f2229c6ec6c04b4
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-10-22 18:23:34 +0530

    Updates

---
+++ precache-manifest.86bb68551fda2520cfcf7dd167197ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9913484a25df97b7f05185a828de53a0",
+    "url": "/quill/index.html"
+  },
+  {
+    "revision": "b2bc36afaf7b3d7c0a08",
+    "url": "/quill/static/css/main.352dde3d.chunk.css"
+  },
+  {
+    "revision": "e3f23bc28d059f2085ca",
+    "url": "/quill/static/js/2.2a3b0e01.chunk.js"
+  },
+  {
+    "revision": "3ecdd6c26c6d0de4a0ebcef66626da48",
+    "url": "/quill/static/js/2.2a3b0e01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2bc36afaf7b3d7c0a08",
+    "url": "/quill/static/js/main.87c1e85e.chunk.js"
+  },
+  {
+    "revision": "5ed5770efc5dcc885ef9",
+    "url": "/quill/static/js/runtime-main.675ec394.js"
+  }
+]);
